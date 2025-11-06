9fbdd44a353e93124d94d89071beb0973d441751
OctaS2711 <octaviosarril19@gmail.com>
2025-11-06 20:24:14 -0300

    Update piedra-papel-tijero.py

--- piedra-papel-tijero.py
+++ piedra-papel-tijero.py
@@ -37,21 +37,27 @@ def elegirMano():
     for x in range(len(manos)):
         print(str(contador) + ": " + str(manos[x][0]))
         contador += 1
-
-    manoElegida = int(input("¿Que mano quiere jugar?\n"))
-
-    return manos[manoElegida]
+    
+    manoElegida = int(input("¿Que mano quiere jugar?\n \n" "JUGADOR: "))
+    while manoElegida <0 or manoElegida > 4:  #controlo que el usuario no coloque seleccione un numero fuera del array (el array es de longitud 5, es decir que puede seleccionar del 0 al 4)
+        print('por favor elegir una mano de las que figuran en pantalla \n Las opciones son del 0 al 4')
+        manoElegida = int(input("¿Que mano quiere jugar?\n \n" "JUGADOR: "))
+    return manos[manoElegida] 
 
 def elegirManoMaquina():
-    manoElegida = random.randint(0, len(manos) - 1)
-    return manos[manoElegida]
-
+    aux = 0
+    manoElegida = random.randint(0, len(manos) - 1) 
+    aux = manos[manoElegida][0]  #el primer corchete marca la fila y el segundo la columna de la matriz, esto me devuelve lo que esta en la columna, es decir la mano que juega el programa
+    return manos[manoElegida], aux #me devuelve 2 valores, el auxiliar y el array
+    
 
 def jugar(puntos, puntosMaquina):
     
-    manoMaquina = elegirManoMaquina()
-    print(manoMaquina) ## TODO: Eliminar este print
+    
+    manoMaquina, aux1 = elegirManoMaquina()
     manoHumana = elegirMano()
+    print('MAQUINA: ',aux1)
+    print()
     
     if manoMaquina == manoHumana:
         return puntos, puntosMaquina
@@ -70,14 +76,46 @@ def jugar(puntos, puntosMaquina):
 
 
 #codigo base
+ronda = 0 #variable contadora
+print('BIENVENIDO A PIEDRA - PAPEL - TIJERA - LAGARTO - SPOCK')
+print('REGLAS: ')
+print()
+print('* PIEDRA APLASTA AL LAGARTO / * PIEDRA ROMPE LA TIJERA')
+print('* PAPEL TAPA LA PIEDRA / * PAPEL DESAUTORIZA A SPOCK')
+print('* TIJERA CORTA AL PAPEL / * TIJERA DECAPITA AL LAGARTO')
+print('* LAGARTO DEVORA AL PAPEL / * LAGARTO ENVENENA A SPOCK')
+print('* SPOCK ROMPE LA TIJERA / * SPOCK VAPORIZA LA PIEDRA')
+print()
 ganaCon = int(input("INGRESE A CUANTOS PUNTOS VA A JUGAR \n"))
-if ganaCon == 0:
+print()
+while ganaCon < 0: #chequeo que no ingresen negativos
+    print('POR FAVOR NO COLOCAR NUMEROS NEGATIVOS\n')
+    ganaCon = int(input("INGRESE A CUANTOS PUNTOS VA A JUGAR \n"))
+    print()
+
+if ganaCon == 0: #si los puntos a jugar vale cero termina el programa
     print("No se juega, fin del programa")
 else:
     puntos = 0
     puntosMaquina = 0
-    while puntos != ganaCon and puntosMaquina != ganaCon:
+    while puntos != ganaCon and puntosMaquina != ganaCon: #ciclo hasta que o los puntos del usuario o de la maquina sean equivalentes a la cantidad de puntos a ganar
+        ronda = ronda + 1
+        print('RONDA N.° ', ronda)
         puntos, puntosMaquina = jugar(puntos, puntosMaquina)
-        print(puntos, puntosMaquina)
-    
+        print('PUNTOS: ',puntos, ' - ', puntosMaquina)
+        print()
+
+    if puntos > puntosMaquina: #comparo los puntos para ver quien gano
+        print('¡¡FELICIDADES, GANASTE!!')
+        print()
+        print('CANTIDAD DE RONDAS: ', ronda)
+    else:
+        print('GANO LA MAQUINA, SUERTE LA PROXIMA')
+        print()
+        print('CANTIDAD DE RONDAS: ', ronda)
+        
+
+
+
+print()
 print("FIN DE JUEGO")
